f20960fb9614c151f61710906b4b520c2b729a7f
DuckDivers <howard@duckdiverllc.com>
2019-03-12 10:12:49 -0400

    J Updates

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -5,6 +5,9 @@ add_action( 'wp_enqueue_scripts', 'wcfg_enqueue_scripts', 12 );
 
 function wcfg_enqueue_scripts() {
 	if ( !is_admin() && is_singular('product') ) {
+		wp_enqueue_script( 'pretty-js', WCFG_URL . '/assets/jquery.prettyPhoto.js', array( 'jquery' ), WCFG_VERSION );
+		wp_enqueue_style( 'pretty-flickity', WCFG_URL . '/assets/prettyPhoto.css', array(), WCFG_VERSION );
+
 		wp_enqueue_script( 'flickity', WCFG_URL . '/assets/flickity.pkgd.min.js', array( 'jquery' ), '2.0.2' );
 		wp_enqueue_style( 'flickity', WCFG_URL . '/assets/flickity.css', array(), '2.0.2' );
 
